09f127cdedb491c9fbc5a31bc06c990c5ec97ba3
uno <st9007a@gmail.com>
2017-12-19 12:03:18 +0800

    Add test case of node matrix add, sub

--- tests/test_node.c
+++ tests/test_node.c
@@ -16,7 +16,7 @@
 #define CHECK_SUCCESS(func) \
     printf("Test "func": Success\n");
 
-void test_node_create_op(float *data, uint32_t *dim, uint32_t num_dims)
+void test_create_op(float *data, uint32_t *dim, uint32_t num_dims)
 {
     Node *test_node = node_variable(dim, num_dims, "var");
 
@@ -50,7 +50,7 @@ void test_node_create_op(float *data, uint32_t *dim, uint32_t num_dims)
     free(test_node);
 }
 
-void test_node_scalar_op(uint32_t *dim, uint32_t num_dims)
+void test_scalar_op(uint32_t *dim, uint32_t num_dims)
 {
     Node *n1 = node_placeholder(dim, num_dims, "node1");
     Node *n2 = node_variable(NULL, 0, "scalar");
@@ -99,13 +99,45 @@ void test_node_scalar_op(uint32_t *dim, uint32_t num_dims)
     free(n2);
 }
 
+void test_matrix_op(uint32_t *dim, uint32_t num_dims)
+{
+    Node *n1 = node_placeholder(dim, num_dims, "node1");
+    Node *n2 = node_variable(dim, num_dims, "node2");
+
+    Node *test_node = node_matrix_add(n1, n2, "matrix_add");
+
+    CHECK("node_matrix_add()", test_node->type == DL_PLACEHOLDER);
+    CHECK("node_matrix_add()", test_node->expr.type == DL_MATRIX_ADD);
+    CHECK("node_matrix_add()", test_node->data.num_dims == n1->data.num_dims);
+    CHECK("node_matrix_add()", !memcmp(test_node->data.dim, n1->data.dim, sizeof(int) * test_node->data.num_dims));
+
+    CHECK_SUCCESS("node_matrix_add()");
+
+    free(test_node);
+
+    test_node = node_matrix_sub(n1, n2, "matrix_sub");
+
+    CHECK("node_matrix_sub()", test_node->type == DL_PLACEHOLDER);
+    CHECK("node_matrix_sub()", test_node->expr.type == DL_MATRIX_SUB);
+    CHECK("node_matrix_sub()", test_node->data.num_dims == n1->data.num_dims);
+    CHECK("node_matrix_sub()", !memcmp(test_node->data.dim, n1->data.dim, sizeof(int) * test_node->data.num_dims));
+
+    CHECK_SUCCESS("node_matrix_sub()");
+
+    free(test_node);
+
+    free(n1);
+    free(n2);
+}
+
 int main()
 {
     float test_data[4] = { 2.0, 2.0, 3.0, 4.0 };
     uint32_t test_dim[2] = { 2, 2 };
 
-    test_node_create_op(test_data, test_dim, 2);
-    test_node_scalar_op(test_dim, 2);
+    test_create_op(test_data, test_dim, 2);
+    test_scalar_op(test_dim, 2);
+    test_matrix_op(test_dim, 2);
 
     return 0;
 }
