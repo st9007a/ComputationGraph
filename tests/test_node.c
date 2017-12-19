ff9774debe441cfeb55c88430d92c36d1f47e32e
uno <st9007a@gmail.com>
2017-12-19 18:56:39 +0800

    Apply new macro in test case of node

--- tests/test_node.c
+++ tests/test_node.c
@@ -11,8 +11,8 @@
 #define TEST_DIM {2, 2}
 #define TEST_NUM_DIMS 2
 
-#define TEST_START \
-    float test_data[4] = TEST_DATA; \
+#define TEST_START                   \
+    float test_data[4] = TEST_DATA;  \
     uint32_t test_dim[2] = TEST_DIM; \
     uint32_t test_num_dims = 2;
 
@@ -20,19 +20,19 @@
 #define TEST_CASE(name) void test_##name(float *data, uint32_t *dim, uint32_t num_dims)
 #define RUN_TEST_CASE(name) test_##name(TEST_ARGS);
 
-#define CHECK_NODE(func, test_node, expect) \
-    do { \
-        CHECK(func, test_node->type == expect.type); \
-        CHECK(func, !strcmp(test_node->name, expect.name)); \
-        \
-        CHECK(func, test_node->data.num_dims == expect.data.num_dims); \
+#define CHECK_NODE(func, test_node, expect)                                                      \
+    do {                                                                                         \
+        CHECK(func, test_node->type == expect.type);                                             \
+        CHECK(func, !strcmp(test_node->name, expect.name));                                      \
+                                                                                                 \
+        CHECK(func, test_node->data.num_dims == expect.data.num_dims);                           \
         CHECK(func, !memcmp(test_node->data.dim, expect.data.dim, sizeof(int) * TEST_NUM_DIMS)); \
-        \
-        CHECK(func, test_node->expr.type == expect.expr.type); \
-        CHECK(func, test_node->expr.args[0] == expect.expr.args[0]); \
-        CHECK(func, test_node->expr.args[1] == expect.expr.args[1]); \
-                \
-        CHECK_SUCCESS(func); \
+                                                                                                 \
+        CHECK(func, test_node->expr.type == expect.expr.type);                                   \
+        CHECK(func, test_node->expr.args[0] == expect.expr.args[0]);                             \
+        CHECK(func, test_node->expr.args[1] == expect.expr.args[1]);                             \
+                                                                                                 \
+        CHECK_SUCCESS(func);                                                                     \
     } while(0);
 
 TEST_CASE(node_variable)
@@ -105,82 +105,184 @@ TEST_CASE(node_constant)
     free(test_node);
 }
 
-void test_scalar_op(uint32_t *dim, uint32_t num_dims)
+TEST_CASE(node_scalar_add)
 {
     Node *n1 = node_placeholder(dim, num_dims, "node1");
     Node *n2 = node_variable(NULL, 0, "scalar");
 
+    Node expect = {
+        .name = "scalar_add",
+        .type = DL_PLACEHOLDER,
+        .data = {
+            .dim = TEST_DIM,
+            .num_dims = TEST_NUM_DIMS,
+        },
+        .grad = {
+            .dim = TEST_DIM,
+            .num_dims = TEST_NUM_DIMS,
+        },
+        .expr = {
+            .type = DL_SCALAR_ADD,
+            .args = {n1, n2},
+        },
+    };
+
     Node *test_node = node_scalar_add(n1, n2, "scalar_add");
+    CHECK_NODE("node_scalar_add()", test_node, expect);
 
-    CHECK("node_scalar_add()", test_node->type == DL_PLACEHOLDER);
-    CHECK("node_scalar_add()", test_node->expr.type == DL_SCALAR_ADD);
-    CHECK("node_scalar_add()", test_node->data.num_dims == n1->data.num_dims);
-    CHECK("node_scalar_add()", !memcmp(test_node->data.dim, n1->data.dim, sizeof(int) * test_node->data.num_dims));
-
-    CHECK_SUCCESS("node_scalar_add()");
     free(test_node);
-
-    test_node = node_scalar_sub(n1, n2, "scalar_sub");
-
-    CHECK("node_scalar_sub()", test_node->type == DL_PLACEHOLDER);
-    CHECK("node_scalar_sub()", test_node->expr.type == DL_SCALAR_SUB);
-    CHECK("node_scalar_sub()", test_node->data.num_dims == n1->data.num_dims);
-    CHECK("node_scalar_sub()", !memcmp(test_node->data.dim, n1->data.dim, sizeof(int) * test_node->data.num_dims));
-
-    CHECK_SUCCESS("node_scalar_sub()");
-    free(test_node);
-
-    test_node = node_scalar_mul(n1, n2, "scalar_mul");
-
-    CHECK("node_scalar_mul()", test_node->type == DL_PLACEHOLDER);
-    CHECK("node_scalar_mul()", test_node->expr.type == DL_SCALAR_MUL);
-    CHECK("node_scalar_mul()", test_node->data.num_dims == n1->data.num_dims);
-    CHECK("node_scalar_mul()", !memcmp(test_node->data.dim, n1->data.dim, sizeof(int) * test_node->data.num_dims));
-
-    CHECK_SUCCESS("node_scalar_mul()");
-    free(test_node);
-
-    test_node = node_scalar_div(n1, n2, "scalar_div");
-
-    CHECK("node_scalar_div()", test_node->type == DL_PLACEHOLDER);
-    CHECK("node_scalar_div()", test_node->expr.type == DL_SCALAR_DIV);
-    CHECK("node_scalar_div()", test_node->data.num_dims == n1->data.num_dims);
-    CHECK("node_scalar_div()", !memcmp(test_node->data.dim, n1->data.dim, sizeof(int) * test_node->data.num_dims));
-
-    CHECK_SUCCESS("node_scalar_div()");
-    free(test_node);
-
     free(n1);
     free(n2);
 }
 
-void test_matrix_op(uint32_t *dim, uint32_t num_dims)
+TEST_CASE(node_scalar_sub)
+{
+    Node *n1 = node_placeholder(dim, num_dims, "node1");
+    Node *n2 = node_variable(NULL, 0, "scalar");
+
+    Node expect = {
+        .name = "scalar_sub",
+        .type = DL_PLACEHOLDER,
+        .data = {
+            .dim = TEST_DIM,
+            .num_dims = TEST_NUM_DIMS,
+        },
+        .grad = {
+            .dim = TEST_DIM,
+            .num_dims = TEST_NUM_DIMS,
+        },
+        .expr = {
+            .type = DL_SCALAR_SUB,
+            .args = {n1, n2},
+        },
+    };
+
+    Node *test_node = node_scalar_sub(n1, n2, "scalar_sub");
+    CHECK_NODE("node_scalar_sub()", test_node, expect);
+
+    free(test_node);
+    free(n1);
+    free(n2);
+}
+
+TEST_CASE(node_scalar_mul)
+{
+    Node *n1 = node_placeholder(dim, num_dims, "node1");
+    Node *n2 = node_variable(NULL, 0, "scalar");
+
+    Node expect = {
+        .name = "scalar_mul",
+        .type = DL_PLACEHOLDER,
+        .data = {
+            .dim = TEST_DIM,
+            .num_dims = TEST_NUM_DIMS,
+        },
+        .grad = {
+            .dim = TEST_DIM,
+            .num_dims = TEST_NUM_DIMS,
+        },
+        .expr = {
+            .type = DL_SCALAR_MUL,
+            .args = {n1, n2},
+        },
+    };
+
+    Node *test_node = node_scalar_mul(n1, n2, "scalar_mul");
+    CHECK_NODE("node_scalar_mul()", test_node, expect);
+
+    free(test_node);
+    free(n1);
+    free(n2);
+}
+
+TEST_CASE(node_scalar_div)
+{
+    Node *n1 = node_placeholder(dim, num_dims, "node1");
+    Node *n2 = node_variable(NULL, 0, "scalar");
+
+    Node expect = {
+        .name = "scalar_div",
+        .type = DL_PLACEHOLDER,
+        .data = {
+            .dim = TEST_DIM,
+            .num_dims = TEST_NUM_DIMS,
+        },
+        .grad = {
+            .dim = TEST_DIM,
+            .num_dims = TEST_NUM_DIMS,
+        },
+        .expr = {
+            .type = DL_SCALAR_DIV,
+            .args = {n1, n2},
+        },
+    };
+
+    Node *test_node = node_scalar_div(n1, n2, "scalar_div");
+    CHECK_NODE("node_scalar_div()", test_node, expect);
+
+    free(test_node);
+    free(n1);
+    free(n2);
+}
+
+TEST_CASE(node_matrix_add)
 {
     Node *n1 = node_placeholder(dim, num_dims, "node1");
     Node *n2 = node_variable(dim, num_dims, "node2");
 
+    Node expect = {
+        .name = "matrix_add",
+        .type = DL_PLACEHOLDER,
+        .data = {
+            .dim = TEST_DIM,
+            .num_dims = TEST_NUM_DIMS,
+        },
+        .grad = {
+            .dim = TEST_DIM,
+            .num_dims = TEST_NUM_DIMS,
+        },
+        .expr = {
+            .type = DL_MATRIX_ADD,
+            .args = {n1, n2},
+        },
+    };
+
     Node *test_node = node_matrix_add(n1, n2, "matrix_add");
 
-    CHECK("node_matrix_add()", test_node->type == DL_PLACEHOLDER);
-    CHECK("node_matrix_add()", test_node->expr.type == DL_MATRIX_ADD);
-    CHECK("node_matrix_add()", test_node->data.num_dims == n1->data.num_dims);
-    CHECK("node_matrix_add()", !memcmp(test_node->data.dim, n1->data.dim, sizeof(int) * test_node->data.num_dims));
-
-    CHECK_SUCCESS("node_matrix_add()");
+    CHECK_NODE("node_matrix_add()", test_node, expect);
 
     free(test_node);
+    free(n1);
+    free(n2);
+}
 
-    test_node = node_matrix_sub(n1, n2, "matrix_sub");
+TEST_CASE(node_matrix_sub)
+{
+    Node *n1 = node_placeholder(dim, num_dims, "node1");
+    Node *n2 = node_variable(dim, num_dims, "node2");
 
-    CHECK("node_matrix_sub()", test_node->type == DL_PLACEHOLDER);
-    CHECK("node_matrix_sub()", test_node->expr.type == DL_MATRIX_SUB);
-    CHECK("node_matrix_sub()", test_node->data.num_dims == n1->data.num_dims);
-    CHECK("node_matrix_sub()", !memcmp(test_node->data.dim, n1->data.dim, sizeof(int) * test_node->data.num_dims));
+    Node expect = {
+        .name = "matrix_sub",
+        .type = DL_PLACEHOLDER,
+        .data = {
+            .dim = TEST_DIM,
+            .num_dims = TEST_NUM_DIMS,
+        },
+        .grad = {
+            .dim = TEST_DIM,
+            .num_dims = TEST_NUM_DIMS,
+        },
+        .expr = {
+            .type = DL_MATRIX_SUB,
+            .args = {n1, n2},
+        },
+    };
 
-    CHECK_SUCCESS("node_matrix_sub()");
+    Node *test_node = node_matrix_sub(n1, n2, "matrix_sub");
+
+    CHECK_NODE("node_matrix_sub()", test_node, expect);
 
     free(test_node);
-
     free(n1);
     free(n2);
 }
@@ -193,6 +295,14 @@ int main()
     RUN_TEST_CASE(node_variable);
     RUN_TEST_CASE(node_constant);
     RUN_TEST_CASE(node_placeholder);
+
+    RUN_TEST_CASE(node_scalar_add);
+    RUN_TEST_CASE(node_scalar_sub);
+    RUN_TEST_CASE(node_scalar_mul);
+    RUN_TEST_CASE(node_scalar_div);
+
+    RUN_TEST_CASE(node_matrix_add);
+    RUN_TEST_CASE(node_matrix_sub);
 
     return 0;
 }
